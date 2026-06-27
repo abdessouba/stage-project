cc6760b0f13c653aa0bdf67b47d2c2fd3c94d039
abdessouba <abdessouba@gmail.com>
2026-06-27 14:53:59 +0100

    Adding Email Verification"

--- src/main/java/com/app/econservatoire/service/EleveService.java
+++ src/main/java/com/app/econservatoire/service/EleveService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import com.app.econservatoire.Repository.EleveRepository;
 import com.app.econservatoire.Repository.PayRepository;
 import com.app.econservatoire.dto.eleve.EleveRequest;
-import com.app.econservatoire.dto.eleve.EleveResponse;
 import com.app.econservatoire.mapper.EleveMapper;
 import com.app.econservatoire.models.Eleve;
 import com.app.econservatoire.models.Pay;
@@ -21,9 +20,10 @@ public class EleveService {
     private final EleveRepository eleveRepository;
     private final PayRepository payRepository;
     private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenService emailVerificationService;
 
 
-    public EleveResponse registerEleve(EleveRequest requestEleve) {
+    public void registerEleve(EleveRequest requestEleve) {
 
         if (eleveRepository.existsByEmail(requestEleve.getEmail())) {
             throw new RuntimeException("User With This Email already exists");
@@ -39,6 +39,7 @@ public class EleveService {
 
         Eleve savedEleve = eleveRepository.save(eleve);
 
-        return eleveMapper.toEntityResponse(savedEleve);
+        // handle email verification after eleve created
+        emailVerificationService.handleVerificationEmail(savedEleve);
     }
 }
